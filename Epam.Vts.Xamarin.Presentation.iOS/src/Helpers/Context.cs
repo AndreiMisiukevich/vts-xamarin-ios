9580b52b067f0eda3bc5f9c6b31e6c2380cec882
Andrei Misiukevich <andrei.misiukevich@gmail.com>
2016-06-20 18:30:35 +0300

    to prev.

---
+++ Epam.Vts.Xamarin.Presentation.iOS/src/Helpers/Context.cs
@@ -0,0 +1,10 @@
+﻿using Epam.Vts.Xamarin.Presentation.iOS.Infrastructure;
+using UIKit;
+
+namespace Epam.Vts.Xamarin.Presentation.iOS.Helpers
+{
+    public static class Context
+    {
+        public static AppDelegate App = UIApplication.SharedApplication.Delegate as AppDelegate;
+    }
+}
